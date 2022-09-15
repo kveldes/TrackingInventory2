e87fda5f4c4eab2f82ba6efb2a95bca759f8e367
Konstantinos Velntes <kveldes@hotmail.com>
2022-09-15 12:09:37 +0300

    Adding the 2 more fields in testSpec and production class

--- inventory2/src/main/java/inventory/Item.java
+++ inventory2/src/main/java/inventory/Item.java
@@ -1,15 +1,22 @@
 package inventory;
 
-import java.math.BigDecimal;
+import java.math.BigInteger;
 
 public class Item {
 
 	private String name;
+	private String serialNumber;
+	private BigInteger amount;
 
-	public Item(String name) {
+	public Item(String name, String serialNumber, BigInteger amount) {
 		nameNotNull(name);
 		nameNotEmptyString(name);
+		serialNumberNotNull(serialNumber);
+		serialNumberNotEmptyString(serialNumber);
+		amountNumberNotNull(amount);
 		this.name = name;
+		this.serialNumber = serialNumber;
+		this.amount = amount;
 	}
 
 	public void setName(String name) {
@@ -17,10 +24,23 @@ public class Item {
 
 	}
 
-
-
 	public String getName() {
 		return name;
+	}
+
+	public void setSerialNumber(String serialNumber) {
+		this.serialNumber = serialNumber;
+
+	}
+
+	public String getSerialNumber() {
+		return serialNumber;
+	}
+
+	private void nameNotNull(String name) {
+		if (name == null) {
+			throw new RuntimeException("Name cannot be null");
+		}
 	}
 
 	private void nameNotEmptyString(String name) {
@@ -29,10 +49,24 @@ public class Item {
 		}
 	}
 
-	private void nameNotNull(String name) {
-		if (name == null) {
-			throw new RuntimeException("Name cannot be null");
+	private void serialNumberNotNull(String serialNumber) {
+		if (serialNumber == null) {
+			throw new RuntimeException("SerialNumber cannot be null");
 		}
+	}
+
+	private void serialNumberNotEmptyString(String serialNumber) {
+		if ("".equals(serialNumber)) {
+			throw new RuntimeException("SerialNumber cannot be empty");
+		}
+
+	}
+
+	private void amountNumberNotNull(BigInteger amount) {
+		if (amount == null) {
+			throw new RuntimeException("Amount cannot be null");
+		}
+
 	}
 
 }
--- inventory2/src/test/groovy/inventory/ItemSpec.groovy
+++ inventory2/src/test/groovy/inventory/ItemSpec.groovy
@@ -7,20 +7,24 @@ class ItemSpec extends Specification{
 
 
 	@Unroll
-	def "item return a name"(){
+	def "item return a Name and a Serial number and a Amount"(){
 		given:
-		Item item = new Item(name)
+		Item item = new Item(name, serialNumber, amount)
 
 		expect:
 		item.name == name
+		item.serialNumber == serialNumber
+		item.amount == amount
 
 		where:
 		name << ["name", "name 2"]
+		serialNumber << ["sn1", "sn2"]
+		amount = BigInteger.valueOf(10);
 	}
 
-	def "name cannot be null"(){
+	def "name  cannot be null"(){
 		when:
-		Item item = new Item(null)
+		Item item = new Item(null,"sn1",10)
 
 		then:
 		Exception e = thrown(Exception)
@@ -29,10 +33,37 @@ class ItemSpec extends Specification{
 
 	def "name cannot be Empty String"(){
 		when:
-		Item item = new Item("")
+		Item item = new Item("","sn1",10)
 
 		then:
 		Exception e = thrown(Exception)
 		e.message == "Name cannot be empty"
+	}
+
+	def "serialNumber  cannot be null"(){
+		when:
+		Item item = new Item("name",null,10)
+
+		then:
+		Exception e = thrown(Exception)
+		e.message == "SerialNumber cannot be null"
+	}
+
+	def "serialNumber cannot be empty String"(){
+		when:
+		Item item = new Item("name","",10)
+
+		then:
+		Exception e = thrown(Exception)
+		e.message == "SerialNumber cannot be empty"
+	}
+
+	def "amount  cannot be null"(){
+		when:
+		Item item = new Item("name","sn1",null)
+
+		then:
+		Exception e = thrown(Exception)
+		e.message == "Amount cannot be null"
 	}
 }
